0df8cb63a98018a4e3129937aea82437116cdd71
Muayyat Billah <billah@muayyat.com>
2023-07-12 11:54:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,74 @@
 
+import tensorflow as tf
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import Model
+
+# GAN Generator
+def build_generator(input_shape):
+    inputs = Input(shape=input_shape)
+    # Define the generator architecture using Convolutional and Upsampling layers
+    # Example architecture:
+    conv1 = Conv2D(64, (3, 3), activation='relu', padding='same')(inputs)
+    conv2 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv1)
+    upsampling1 = UpSampling2D((2, 2))(conv2)
+    conv3 = Conv2D(64, (3, 3), activation='relu', padding='same')(upsampling1)
+    conv4 = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(conv3)
+    outputs = conv4
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+# CNN for Reflection Mask Estimation
+def build_cnn(input_shape):
+    inputs = Input(shape=input_shape)
+    # Define the CNN architecture for reflection mask estimation
+    # Example architecture:
+    conv1 = Conv2D(64, (3, 3), activation='relu', padding='same')(inputs)
+    conv2 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv1)
+    pooling = MaxPooling2D((2, 2))(conv2)
+    conv3 = Conv2D(64, (3, 3), activation='relu', padding='same')(pooling)
+    conv4 = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(conv3)
+    outputs = conv4
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+# Define the GAN
+def build_gan(generator, cnn):
+    # Fix the weights of the CNN during GAN training
+    cnn.trainable = False
+    # Connect the generator and the CNN
+    inputs = generator.input
+    outputs = cnn(generator(inputs))
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+# Define the input shape (assuming grayscale images of size 128x128)
+input_shape = (128, 128, 1)
+
+# Build the generator, CNN, and GAN
+generator = build_generator(input_shape)
+cnn = build_cnn(input_shape)
+gan = build_gan(generator, cnn)
+
+# Compile the GAN
+gan.compile(loss='binary_crossentropy', optimizer='adam')
+
+# Training loop
+# Assuming you have training data X_train and reflection masks M_train
+# Example code for training iterations:
+for epoch in range(num_epochs):
+    # Generate fake reflections using the generator
+    fake_reflections = generator.predict(X_train)
+    
+    # Train the discriminator (CNN) on real and fake reflection masks
+    cnn.trainable = True
+    cnn.train_on_batch(X_train, M_train)
+    cnn.train_on_batch(fake_reflections, np.zeros_like(fake_reflections))
+    
+    # Train the generator (GAN) to fool the discriminator
+    cnn.trainable = False
+    gan.train_on_batch(X_train, M_train)
+    
+    # Print training progress or save intermediate outputs
+
+# After training, you can use the generator for reflection removal
+reflection_free_images = generator.predict(test_images)
