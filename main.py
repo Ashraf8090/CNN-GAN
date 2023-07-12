cca7869376614446995c81c2bc38763785a2d3bd
Muayyat Billah <billah@muayyat.com>
2023-07-12 12:01:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+import os
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import Model
@@ -52,9 +53,26 @@ gan = build_gan(generator, cnn)
 # Compile the GAN
 gan.compile(loss='binary_crossentropy', optimizer='adam')
 
+# Prepare data paths
+training_dir = 'training'
+model_dir = 'model'
+input_dir = 'input'
+results_dir = 'results'
+
+# Load training data
+X_train = []
+M_train = []
+for filename in os.listdir(training_dir):
+    if filename.endswith('.png'):
+        image = tf.keras.preprocessing.image.load_img(os.path.join(training_dir, filename), target_size=input_shape[:2], color_mode='grayscale')
+        reflection_mask = tf.keras.preprocessing.image.load_img(os.path.join(training_dir, filename.replace('.png', '_mask.png')), target_size=input_shape[:2], color_mode='grayscale')
+        X_train.append(tf.keras.preprocessing.image.img_to_array(image))
+        M_train.append(tf.keras.preprocessing.image.img_to_array(reflection_mask))
+X_train = np.array(X_train) / 255.0
+M_train = np.array(M_train) / 255.0
+
 # Training loop
-# Assuming you have training data X_train and reflection masks M_train
-# Example code for training iterations:
+num_epochs = 10  # Adjust the number of epochs as needed
 for epoch in range(num_epochs):
     # Generate fake reflections using the generator
     fake_reflections = generator.predict(X_train)
@@ -70,5 +88,27 @@ for epoch in range(num_epochs):
     
     # Print training progress or save intermediate outputs
 
-# After training, you can use the generator for reflection removal
-reflection_free_images = generator.predict(test_images)
+# Save the model
+os.makedirs(model_dir, exist_ok=True)
+model_path = os.path.join(model_dir, 'reflection_removal_model.h5')
+gan.save(model_path)
+print(f"Model saved at {model_path}")
+
+# Load the model
+gan = tf.keras.models.load_model(model_path)
+
+# Inference on test images
+os.makedirs(results_dir, exist_ok=True)
+for filename in os.listdir(input_dir):
+    if filename.endswith('.png'):
+        image = tf.keras.preprocessing.image.load_img(os.path.join(input_dir, filename), target_size=input_shape[:2], color_mode='grayscale')
+        input_image = tf.keras.preprocessing.image.img_to_array(image) / 255.0
+        input_image = np.expand_dims(input_image, axis=0)
+        predicted_mask = gan.predict(input_image)
+        predicted_mask = np.squeeze(predicted_mask, axis=0)
+        predicted_mask *= 255.0
+        predicted_mask = predicted_mask.astype(np.uint8)
+        output_image = Image.fromarray(predicted_mask, mode='L')
+        output_path = os.path.join(results_dir, f"{filename}_result.png")
+        output_image.save(output_path)
+        print(f"Result saved at {output_path}")
