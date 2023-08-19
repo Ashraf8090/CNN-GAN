dca9f0615ebfde16cddd6691c9a5dbb607cb5515
Muayyat Billah <billah@muayyat.com>
2023-08-19 14:08:19 +0100

    Update lossfunction.py

--- lossfunction.py
+++ lossfunction.py
@@ -43,7 +43,74 @@ def build_generator(input_shape):
     generator = Model(inputs, output, name='Generator')
     return generator
 
-# ... Rest of the code ...
+# CNN for Reflection Mask Estimation
+def build_cnn(input_shape):
+    inputs = Input(shape=input_shape)
+
+    # CNN architecture
+    x = Conv2D(64, (3, 3), activation='relu', padding='same')(inputs)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+    x = MaxPooling2D((2, 2))(x)
+
+    x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+    x = MaxPooling2D((2, 2))(x)
+
+    x = Conv2D(256, (3, 3), activation='relu', padding='same')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same')(x)
+
+    x = Flatten()(x)
+    x = Dense(1024, activation='relu')(x)
+    x = Dense(1, activation='sigmoid')(x)
+
+    cnn = Model(inputs, x, name='CNN')
+    return cnn
+
+# Define the GAN
+def build_gan(generator, cnn):
+    input_shape = generator.input_shape[1:]  # Assuming the same input shape for both generator and cnn
+    inputs = Input(shape=input_shape)
+
+    # GAN architecture
+    reflection_mask = cnn(inputs)
+    reflection_free_image = generator([inputs, reflection_mask])
+
+    gan = Model(inputs, reflection_free_image, name='GAN')
+    return gan
+# Loss functions
+adversarial_loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+mse_loss = tf.keras.losses.MeanSquaredError()
+perceptual_loss = tf.keras.losses.MeanSquaredError()
+
+lambda_adversarial = 1.0
+lambda_mse = 100.0
+lambda_perceptual = 10.0
+lambda_tv = 1.0
+
+# GAN loss (adversarial loss)
+def gan_loss(y_true, y_pred):
+    return lambda_adversarial * adversarial_loss(y_true, y_pred)
+
+# Reflection mask loss (binary cross-entropy)
+def mask_loss(y_true, y_pred):
+    return lambda_mse * mse_loss(y_true, y_pred)
+
+# Image content loss (perceptual loss)
+def content_loss(y_true, y_pred):
+    # Pre-trained VGG or ResNet model
+    # Example: pre_trained_model = tf.keras.applications.VGG16(include_top=False, weights='imagenet')
+    # Example: feature_true = pre_trained_model(y_true)
+    # Example: feature_pred = pre_trained_model(y_pred)
+    return lambda_perceptual * perceptual_loss(feature_true, feature_pred)
+
+# Total Variation Loss
+def tv_loss(y_pred):
+    return lambda_tv * tf.image.total_variation(y_pred)
+
+# Final loss function for the GAN
+def generator_loss(y_true, y_pred):
+    return gan_loss(y_true, y_pred) + mask_loss(y_true, y_pred) + content_loss(y_true, y_pred) + tv_loss(y_pred)
+
 
 # Calculate PSNR
 def psnr(y_true, y_pred):
