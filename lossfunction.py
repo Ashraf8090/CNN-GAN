4021bd5b069cbe4e03f42fd922aa3402eb625372
Muayyat Billah <billah@muayyat.com>
2023-08-19 14:05:01 +0100

    Update lossfunction.py

--- lossfunction.py
+++ lossfunction.py
@@ -1,35 +1,88 @@
+import os
+import numpy as np
 import tensorflow as tf
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import Model
+from PIL import Image, ImageDraw, ImageFont
 
-# Define loss functions
-adversarial_loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-mse_loss = tf.keras.losses.MeanSquaredError()
-perceptual_loss = tf.keras.losses.MeanSquaredError()
+# GAN Generator
+def build_generator(input_shape):
+    inputs = Input(shape=input_shape)
 
-# Weighting of the different losses
-lambda_adversarial = 1.0
-lambda_mse = 100.0
-lambda_perceptual = 10.0
+    # Encoder
+    x = Conv2D(64, (3, 3), activation='relu', padding='same')(inputs)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+    skip_conn = x
 
-# GAN loss (adversarial loss)
-def gan_loss(y_true, y_pred):
-    return lambda_adversarial * adversarial_loss(y_true, y_pred)
+    # Downsampling
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+    skip_conn2 = x
 
-# Reflection mask loss (binary cross-entropy)
-def mask_loss(y_true, y_pred):
-    return lambda_mse * mse_loss(y_true, y_pred)
+    # Downsampling
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same')(x)
 
-# Image content loss (perceptual loss)
-def content_loss(y_true, y_pred):
-    # Pre-trained VGG or ResNet model
-    # Example: pre_trained_model = tf.keras.applications.VGG16(include_top=False, weights='imagenet')
-    # Example: feature_true = pre_trained_model(y_true)
-    # Example: feature_pred = pre_trained_model(y_pred)
-    return lambda_perceptual * perceptual_loss(feature_true, feature_pred)
+    # Upsampling
+    x = UpSampling2D((2, 2))(x)
+    x = concatenate([x, skip_conn2], axis=-1)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
 
-# Total Variation Loss
-def tv_loss(y_pred):
-    return lambda_tv * tf.image.total_variation(y_pred)
+    # Upsampling
+    x = UpSampling2D((2, 2))(x)
+    x = concatenate([x, skip_conn], axis=-1)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
 
-# Final loss function for the GAN
-def generator_loss(y_true, y_pred):
-    return gan_loss(y_true, y_pred) + mask_loss(y_true, y_pred) + content_loss(y_true, y_pred) + tv_loss(y_pred)
+    # Output
+    output = Conv2D(1, (1, 1), activation='sigmoid', padding='same')(x)
+
+    generator = Model(inputs, output, name='Generator')
+    return generator
+
+# ... Rest of the code ...
+
+# Calculate PSNR
+def psnr(y_true, y_pred):
+    max_value = 1.0  # Assuming pixel values are in the range [0, 1]
+    return tf.image.psnr(y_true, y_pred, max_val=max_value)
+
+# Calculate SSIM
+def ssim(y_true, y_pred):
+    return tf.image.ssim(y_true, y_pred, max_val=1.0)
+
+# Function to annotate image with PSNR and SSIM values
+def annotate_image_with_metrics(image, psnr_value, ssim_value):
+    image = image.copy()
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.load_default()  # You can use a custom font if needed
+    draw.text((10, 10), f"PSNR: {psnr_value:.2f}", font=font, fill='white')
+    draw.text((10, 30), f"SSIM: {ssim_value:.4f}", font=font, fill='white')
+    return image
+
+# ... Rest of the code ...
+
+# Perform reflection removal on input images (e.g., in /input directory)
+input_images_dir = 'input'
+os.makedirs(output_dir, exist_ok=True)
+for filename in os.listdir(input_images_dir):
+    if filename.endswith('.png'):
+        image = tf.keras.preprocessing.image.load_img(os.path.join(input_images_dir, filename), target_size=input_shape[:2], color_mode='grayscale')
+        input_image = tf.keras.preprocessing.image.img_to_array(image) / 255.0
+        reflection_free_image = remove_reflections(np.expand_dims(input_image, axis=0), generator)[0]
+
+        # Calculate PSNR and SSIM between original input image and reflection-free image
+        psnr_value = psnr(np.expand_dims(input_image, axis=0), reflection_free_image)
+        ssim_value = ssim(np.expand_dims(input_image, axis=0), reflection_free_image)
+
+        # Annotate the reflection-free image with PSNR and SSIM values
+        annotated_image = annotate_image_with_metrics(Image.fromarray((reflection_free_image * 255).astype(np.uint8)), psnr_value, ssim_value)
+
+        # Save reflection-free image with annotations
+        output_path = os.path.join(output_dir, f"reflection_free_{filename}")
+        annotated_image.save(output_path)
+
+print("Reflection removal completed and annotated output images saved.")
